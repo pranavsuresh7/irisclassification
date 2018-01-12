d53c3507d871ae604d7ace9c1f61b8f402660439
pranavsuresh7 <pranav2413714@gmail.com>
2018-01-13 00:44:15 +0530

    Update IrisCost.m

--- IrisCost.m
+++ IrisCost.m
@@ -1,8 +1,9 @@
 function [J,grad] = IrisCost(theta,X,y,lambda)
-%number of training set
+%number of training example in dataset 
 m=size(X,1);
- 
+ %gradient should have  same size as that of theta 
  grad=zeros(size(theta));
+ 
  J=0;
  %regularized costfunction 
 J=(1/m)*sum(-y'*log(sigmoid(X*theta))-(1-y)'*log(1-sigmoid(X*theta))) +(lambda/(2*m))*sum((theta(2:length(theta))).*theta(2:length(theta)));
